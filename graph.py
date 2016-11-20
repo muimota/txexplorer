02d08d10cf658d028c07ae25b984e4a3fc4dfdcb
Martin Nadal <martin.nadal@gmail.com>
2016-11-21 01:16:38 +0900

    basic convert

--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 
 from collections import Counter
 from utils import *
+from tqdm import tqdm
 import cPickle as pickle
 #devuelve
 
@@ -8,63 +9,67 @@ import cPickle as pickle
 def getStepData(inputs):
 
     stepInputs = Counter()
-    addresses = set()
-    coinbase  = set()
+    addresses  = set()
+    coinbases  = set()
 
-    for input in inputs:
+    for input in tqdm(inputs):
 
         addressId,txId = input
         value          = inputs[input]
 
         tx = getTransaction(txId)
         if 'isCoinBase' in tx:
-            coinbase.add(addressId)
+            coinbases.add(addressId)
             continue
 
         newInputs = breakdownInput(tx,addressId,value)
+        #print '>{} >{}'.format(txId,addressId)
         for newInput in newInputs:
+            if newInput == None:
+                continue
             addressId,txId = newInput
             addresses.add(addressId)
         #aqui
         stepInputs += newInputs
 
-    stepdata = {'inputs':stepInputs,'addresses':addresses,'coinbase':coinbase}
+    stepdata = {'inputs':stepInputs,'addresses':addresses,'coinbases':coinbases}
     return stepdata
 
 if __name__ == '__main__':
 
     txId = '23fae8e2913669fd76c0f1ac5fe9a1e50b2c857cc317937edb2b2d3dfcf0b252'
     filename = 'tx_{}.pickle'.format(txId[-5:])
-    resetPickle = False
+    resetPickle = True
 
     try:
         if resetPickle:
             raise Exception()
-
         data = pickle.load(open(filename,'rb'))
-    except:
+        #recalculate last 2 steps in case last is corrupted
+        inputs = data['stepdata'][-2]['inputs']
+    except Exception as e:
+        print e
         print '{} file not found!'.format(filename)
         data = {}
         data['transactionId'] = txId
         data['stepdata'] = []
+        tx = getTransaction(txId)
+        inputs = breakdownInput(tx)
 
     startStep = len(data['stepdata'])
     print 'startStep:{}'.format(startStep)
 
-
-    tx = getTransaction(txId)
-    inputs = breakdownInput(tx)
-    for step in range(startStep,50):
+    for step in range(startStep,3):
 
         stepdata  = getStepData(inputs)
-        coinbase  = stepdata['coinbase']
+        coinbases = stepdata['coinbases']
         addresses = stepdata['addresses']
         inputs    = stepdata['inputs']
 
         data['stepdata'].append(stepdata)
         pickle.dump(data,open(filename,'wb'))
 
-        print "step:{} inputs:{} sumValue:{} address:{} coinbase:{}".format(step,len(inputs),sum(inputs.values()),len(addresses),len(coinbase))
+        print "step:{} inputs:{} sumValue:{} addresses:{} coinbases:{}".format(step,len(inputs),sum(inputs.values()),len(addresses),len(coinbases))
 
         if len(inputs) == 0:
             break
--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import requests
+import cPickle as pickle
+import json
 from collections import Counter
+
 
 BASE_URL = 'http://localhost:3001/insight-api/'
 
@@ -28,8 +31,20 @@ def convertJson(data,f = None):
     """ converts data so it can be stored in a json file
     -- data dict with txId, stepdata
     -- f file handler to write, if no defined return json"""
-    pass
+
+    data = data.copy()
+
+    for stepdata in data['stepdata']:
+        
+        del(stepdata['inputs'])
+        for key in ('addresses','coinbases'):
+            stepdata[key] = list(stepdata[key])
+
+    if f != None:
+        json.dump(data,f)
 
 if __name__ == '__main__':
-    tx = getTransaction('28ff7d1e166c111bba69bf66f41f02e3d211bf215f7b82cf440c72c2dcc1b974')
-    print breakdownInput(tx)
+
+    filename = 'tx_0b252'
+    data = pickle.load(open(filename+'.pickle','rb'))
+    convertJson(data,open(filename+'.json','wb'))
