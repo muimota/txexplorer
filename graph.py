b185427c0a8927e197999b74c744c327295a5988
Martin Nadal <martin.nadal@gmail.com>
2016-11-21 02:54:47 +0900

    fixed problem when resuming

--- graph.py
+++ graph.py
@@ -19,15 +19,19 @@ def getStepData(inputs):
 
         tx = getTransaction(txId)
         if 'isCoinBase' in tx:
+            if addressId == None:
+                continue
             coinbases.add(addressId)
             continue
 
         newInputs = breakdownInput(tx,addressId,value)
         #print '>{} >{}'.format(txId,addressId)
         for newInput in newInputs:
-            if newInput == None:
-                continue
+
             addressId,txId = newInput
+            #cases where is not parsed corectly like b039d4a15f5b8e10bcec5384da8995f134fb75e1e1da81e82d25c800e224fa16
+            if addressId == None:
+                continue
             addresses.add(addressId)
         #aqui
         stepInputs += newInputs
@@ -38,15 +42,15 @@ def getStepData(inputs):
 if __name__ == '__main__':
 
     txId = '23fae8e2913669fd76c0f1ac5fe9a1e50b2c857cc317937edb2b2d3dfcf0b252'
-    filename = 'tx_{}.pickle'.format(txId[-5:])
-    resetPickle = True
+    filename = 'tx_{}'.format(txId[-5:])
+    resetPickle = False
 
     try:
         if resetPickle:
             raise Exception()
-        data = pickle.load(open(filename,'rb'))
+        data = pickle.load(open(filename+'.pickle','rb'))
         #recalculate last 2 steps in case last is corrupted
-        inputs = data['stepdata'][-2]['inputs']
+        inputs = data['stepdata'][-1]['inputs']
     except Exception as e:
         print e
         print '{} file not found!'.format(filename)
@@ -56,10 +60,10 @@ if __name__ == '__main__':
         tx = getTransaction(txId)
         inputs = breakdownInput(tx)
 
-    startStep = len(data['stepdata'])
+    startStep = len(data['stepdata'])-1
     print 'startStep:{}'.format(startStep)
 
-    for step in range(startStep,3):
+    for step in range(startStep,50):
 
         stepdata  = getStepData(inputs)
         coinbases = stepdata['coinbases']
@@ -67,9 +71,12 @@ if __name__ == '__main__':
         inputs    = stepdata['inputs']
 
         data['stepdata'].append(stepdata)
-        pickle.dump(data,open(filename,'wb'))
+        pickle.dump(data,open(filename+'.pickle','wb'))
 
         print "step:{} inputs:{} sumValue:{} addresses:{} coinbases:{}".format(step,len(inputs),sum(inputs.values()),len(addresses),len(coinbases))
 
         if len(inputs) == 0:
             break
+
+    print 'saving...'
+    convertJson(data,open(filename+'.json','wb'))
